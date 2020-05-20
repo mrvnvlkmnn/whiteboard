49739ec34539d06a54e739df6ec485b99f834c62
muffinpsd <marvin.volkmann.mv@gmail.com>
2020-05-20 16:11:13 +0200

    Schema::defaultStringLength(191)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Project;
 use App\Observers\ProjectObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Project::observe(ProjectObserver::class);
     }
 }
