a27f89e41a6056b7ae95a1b8cc61948cbda1a1fc
muffinpsd <marvin.volkmann.mv@gmail.com>
2020-10-08 17:48:31 +0200

    updated everything

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+        colors:{
+            'eae-dark': '#000F4D',
+            'eae-light': '#D4E6F6',
+        },},
+  },
+  variants: {},
+  plugins: [],
+}
